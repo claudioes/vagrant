6d1673de0c2371a75833bc5fb9c176a5fb8028ba
Claudio Escobar <claudioe0407@gmail.com>
2018-01-22 16:35:14 -0300

    Se cambia a una ruta absoluta en el script de pyafipws

--- scripts/pyafipws.sh
+++ scripts/pyafipws.sh
@@ -26,33 +26,37 @@ if ! which 'virtualenv' &> /dev/null ; then
     apt-get install -y virtualenv
 fi
 
-folder="~/pyafipws"
-env="~/pyafipenv"
+folder="/home/vagrant/pyafipws"
+env="/home/vagrant/pyafipenv"
 user="vagrant"
 
-if [ ! -d $folder ]; then
+if [ ! -d "$folder" ]; then
     git clone https://github.com/reingart/pyafipws.git $folder
 
-    if [ -d $folder ]; then
-        chown -R $user:$user $folder
+    if [ -d "$folder" ]; then
+        chown -R $user:$user "$folder"
         mkdir "${folder}/cache"
         chmod 775 "${folder}/cache"
         chown $user:www-data "${folder}/cache"
     else
         echo "No se pudo clonar el repositorio de PyAfipWS"
     fi
+else
+    echo "PyAfipWS ya se encuentra instalado"
 fi
 
-if [ ! -d $env ]; then
-    virtualenv $env -p python
+if [ ! -d "$env" ]; then
+    virtualenv "$env" -p python
 
-    if [ -d $env ]; then
+    if [ -d "$env" ]; then
         source "$env/bin/activate"
         pip install -r "${folder}/requirements.txt"
         pip install httplib2==0.9.2
         deactivate
-        chown -R $user:$user $env
+        chown -R $user:$user "$env"
     else
         echo "No se pudo crear el entorno virtual $env"
     fi
+else
+    echo "Entorno $env ya se encuentra instalado"
 fi
